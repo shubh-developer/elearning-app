e80ce9eb949999b87ca050077a2e80c150eb0470
shubh4-coder <shubhamghule358@gmail.com>
2023-04-08 15:15:17 +0530

    modified java content

---
+++ src/BillPay/Test.java
@@ -0,0 +1,13 @@
+package BillPay;
+
+public class Test {
+ public static void main(String[] args) {
+	CreditcardPayment ccd = new CreditcardPayment();
+	BillCollector collector = new BillCollector();
+	
+	collector.setPayment(ccd);
+	
+	collector.collect
+	
+}
+}
